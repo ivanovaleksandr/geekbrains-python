b3eada11431721357040c532dfa350b87344cfc9
ivanov-aleksandr <inter06@mail.ru>
2020-10-07 22:23:09 +0300

    Урок 1

---
+++ lesson 1/lesson 1.4.py
@@ -0,0 +1,12 @@
+"""4. Пользователь вводит целое положительное число.
+Найдите самую большую цифру в числе.
+Для решения используйте цикл while и арифметические операции."""
+
+number = int(input("Введите число: "))
+max_num = number % 10
+number = number // 10
+while number > 0:
+    if number % 10 > max_num:
+        max_num = number % 10
+    number = number // 10
+print(max_num)
