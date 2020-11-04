a693170f421dc462a003db0e226326399454f382
ivanovaleksandr <70534387+ivanovaleksandr@users.noreply.github.com>
2020-11-04 22:17:21 +0300

    Add files via upload
    Урок 8

---
+++ lesson 8.4-6.py
@@ -0,0 +1,75 @@
+"""4. Начните работу над проектом «Склад оргтехники». Создайте класс, описывающий склад. А также класс «Оргтехника»,
+который будет базовым для классов-наследников. Эти классы — конкретные типы оргтехники (принтер, сканер, ксерокс).
+В базовом классе определить параметры, общие для приведенных типов. В классах-наследниках реализовать параметры,
+уникальные для каждого типа оргтехники.
+
+5. Продолжить работу над первым заданием. Разработать методы, отвечающие за приём оргтехники на склад и передачу
+в определенное подразделение компании. Для хранения данных о наименовании и количестве единиц оргтехники, а также
+других данных, можно использовать любую подходящую структуру, например словарь.
+
+6. Продолжить работу над вторым заданием. Реализуйте механизм валидации вводимых пользователем данных.
+Например, для указания количества принтеров, отправленных на склад, нельзя использовать строковый тип данных.
+Подсказка: постарайтесь по возможности реализовать в проекте «Склад оргтехники» максимум возможностей,
+изученных на уроках по ООП."""
+
+
+class OfficeEquipmentWarehouse:
+
+    def __init__(self, name, price, quantity, number_of_lists, *args):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.numb = number_of_lists
+        self.my_store_full = []
+        self.my_store = []
+        self.my_unit = {'Модель устройства': self.name, 'Цена за ед': self.price, 'Количество': self.quantity}
+
+    def __str__(self):
+        return f'{self.name} цена {self.price} количество {self.quantity}'
+
+    def reception(self):
+        try:
+            unit = input(f'Введите наименование ')
+            unit_p = int(input(f'Введите цену за ед '))
+            unit_q = int(input(f'Введите количество '))
+            unique = {'Модель устройства': unit, 'Цена за ед': unit_p, 'Количество': unit_q}
+            self.my_unit.update(unique)
+            self.my_store.append(self.my_unit)
+            print(f'Текущий список -\n {self.my_store}')
+        except:
+            return f'Ошибка ввода данных'
+
+        print(f'Для выхода - Q, продолжение - Enter')
+        q = input(f'---> ')
+        if q == 'Q' or q == 'q':
+            self.my_store_full.append(self.my_store)
+            print(f'Весь склад -\n {self.my_store_full}')
+            return f'Выход'
+        else:
+            return OfficeEquipmentWarehouse.reception(self)
+
+
+class Printer(OfficeEquipmentWarehouse):
+    def to_print(self):
+        return self.numb
+
+
+class Scanner(OfficeEquipmentWarehouse):
+    def to_scan(self):
+        return self.numb
+
+
+class Copier(OfficeEquipmentWarehouse):
+    def to_copier(self):
+        return self.numb
+
+
+unit_1 = Printer('HP', 600, 5, 10)
+unit_2 = Scanner('Canon', 900, 5, 10)
+unit_3 = Copier('Xerox', 1000, 1, 15)
+print(unit_1.reception())
+print(unit_2.reception())
+print(unit_3.reception())
+print(unit_1.to_print())
+print(unit_2.to_scan())
+print(unit_3.to_copier())
